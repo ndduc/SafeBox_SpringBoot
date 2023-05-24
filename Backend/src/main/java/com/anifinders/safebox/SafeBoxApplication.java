289f367db0647e01238d56b58707107874fb9f69
ndduc <ndduc1856@gmail.com>
2023-05-23 20:32:57 -0700

    added docker

--- Backend/src/main/java/com/anifinders/safebox/SafeBoxApplication.java
+++ Backend/src/main/java/com/anifinders/safebox/SafeBoxApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SafeBoxApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SafeBoxApplication.class, args);
+		try {
+			SpringApplication.run(SafeBoxApplication.class, args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
